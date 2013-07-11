ffdfdecbf8c7aa1ee72152d401305c04415c2cf7
mgp <mgp-code@linuxupc.upc.es>
2013-07-12 00:26:59 +0200

    Added URL functionality to grader.js

--- grader.js
+++ grader.js
@@ -24,6 +24,7 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
 
@@ -40,6 +41,10 @@ var cheerioHtmlFile = function(htmlfile) {
     return cheerio.load(fs.readFileSync(htmlfile));
 };
 
+var cheerioUrl = function(data) {
+    return cheerio.load(data);
+};  
+
 var loadChecks = function(checksfile) {
     return JSON.parse(fs.readFileSync(checksfile));
 };
@@ -55,6 +60,20 @@ var checkHtmlFile = function(htmlfile, checksfile) {
     return out;
 };
 
+var checkUrl = function(url, checksfile, callback) {
+    var checks = loadChecks(checksfile).sort();
+    rest.get(program.url).on('complete', function(data) {
+	var out = {};
+	$=cheerioUrl(data);
+	for(var ii in checks) {
+            var present = $(checks[ii]).length > 0;
+            out[checks[ii]] = present;
+    }
+    callback(out);
+    });
+};
+
+
 var clone = function(fn) {
     // Workaround for commander.js issue.
     // http://stackoverflow.com/a/6772648
@@ -62,13 +81,21 @@ var clone = function(fn) {
 };
 
 if(require.main == module) {
-    program
+      program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+	.option('-u, --url <html_file>', 'Link to file')
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+
+  if (program.url) {
+      checkUrl(program.url, program.checks, function(data){
+	  console.log(JSON.stringify(data, null, 4));
+      });
+  } else {
+      var checkJson = checkHtmlFile(program.file, program.checks);
+      var outJson = JSON.stringify(checkJson, null, 4);
+      console.log(outJson);
+  };
 } else {
     exports.checkHtmlFile = checkHtmlFile;
-}
+};
