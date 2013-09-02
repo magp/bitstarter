2a665889e7b88173df14faa7d336e014ee3d92a0
mgp <mgp-code@linuxupc.upc.es>
2013-09-02 19:04:30 +0200

    Added supporters page

--- web.js
+++ web.js
@@ -3,12 +3,16 @@ var fs = require('fs');
 
 var app = express.createServer(express.logger());
 var message = new Buffer(fs.readFileSync('index.html','utf8'));
+var supporters = new Buffer(fs.readFileSync('supporters.html','utf8'));
 
 console.log(message.length);
 console.log(message.toString('utf8'));
 
 app.get('/', function(request, response) {
   response.send(message.toString('utf8'));
+});
+app.get('/supporters', function(request, response) {
+  response.send(supporters.toString('utf8'));
 });
 
 var port = process.env.PORT || 8080;
